ab2db02d3d37838774a008253e7ed5291ea4dc13
Nadeyja <nadia.hamerlak@gmail.com>
2023-03-09 00:54:28 +0100

    Better looking code

--- rpq/rpq.cpp
+++ rpq/rpq.cpp
@@ -5,8 +5,14 @@ int main()
 {
     int N, R[100], P[100], Q[100], X[100];
     ifstream f("data.txt");
-    string s; while (s != "data.1") f>>s;
-    f>>N; for (int i = 0;i < N;i++) f>>R[i]>>P[i]>>Q[i];
+    string s; 
+    while (s != "data.1") {
+        f >> s;
+    }
+    f>>N; 
+    for (int i = 0;i < N;i++) {
+        f >> R[i] >> P[i] >> Q[i];
+    }
     f.close();
 
     cout << N << endl;
